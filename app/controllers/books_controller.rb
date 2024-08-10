3f8403a8510d45f7a86f91ada66bd1cd2b78cf65
EC2 Default User <ec2-user@ip-172-31-18-95.ap-northeast-1.compute.internal>
2024-08-10 11:36:37 +0000

    first commit and finish to create bookers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new  # indexページでのCSS切り替え用
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:success] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      flash_with_error
+      @books = Book.all
+      render :index,  status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:success] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      flash_with_error
+      render :edit, status: :unprocessable_entity  # turboに適切なステータスコードを渡す
+    end
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+    
+    def flash_with_error
+      flash.now[:alert] = "#{@book.errors.count} error".pluralize + " prohibited this book from begin saved:"
+      flash.now[:error_messages] = @book.errors.full_messages
+    end
+end
